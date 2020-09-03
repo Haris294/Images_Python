7dadb8067b06155ccbaa9f7bb32c652c51ff7d90
Haris294 <haris294@gmail.com>
2020-09-04 00:08:49 +0530

    IMAGE TO SKETCH CONVERSION

---
+++ image_to_sketch.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+import imageio
+# from scipy.ndimage import filters
+from scipy.ndimage import gaussian_filter
+img = 'demo.jpg'
+
+def greyscale(rgb):
+    
+    return np.dot(rgb[...,:3],[0.299,0.587,0.114])
+
+def dodge(front,back):
+    result = front*255/(255-back)
+    result[result>255]=255
+    result[back==255]=255
+    return result.astype('uint8')
+
+i = imageio.imread(img)
+g = greyscale(i)
+s = 255-g
+b = gaussian_filter(s,sigma=10)
+r = dodge(b,g)
+
+cv2.imwrite('sketch.png',r)
