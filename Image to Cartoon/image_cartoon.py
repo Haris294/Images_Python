ac03bcc2313c95a3712dadad4cd342fe719b6f5b
Haris294 <haris294@gmail.com>
2020-12-26 23:25:07 +0530

    Update image_cartoon.py

--- Image to Cartoon/image_cartoon.py
+++ Image to Cartoon/image_cartoon.py
@@ -8,7 +8,6 @@ gray = cv2.medianBlur(gray, 5)
 edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
 
 color = cv2.bilateralFilter(img, 9, 300, 300)
-
 cartoon = cv2.bitwise_and(color, color, mask = edges)
 
 cv2.imshow("Image", img)
